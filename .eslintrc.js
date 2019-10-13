bc9370669367b5e36cf7740dad136ab55fec25d3
Emmanuel <emmanuel.ainaj@gmail.com>
2019-10-13 12:27:43 +0100

    edit eslintrc to allow semicolon and disallow space before parenthesis add link-style style to global button styles restructure vue store for multiple stores create modal component add modal to all vehicle page

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,15 @@ module.exports = {
     },
     'extends': [
         'plugin:vue/essential',
-        "eslint:recommended",
+        'eslint:recommended',
         '@vue/standard'
     ],
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        indent: ['error', 4]
+        indent: ['error', 4],
+        'semi': [2, 'always'],
+        'space-before-function-paren': ['error', 'never'],
     },
     parserOptions: {
         parser: 'babel-eslint'
